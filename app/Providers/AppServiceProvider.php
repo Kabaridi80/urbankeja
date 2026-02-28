13d898683b240ccafe4025180174c1961a4282b6
joseph kenindia <brayokabaridi5511@gmail.com>
2026-02-28 11:57:44 +0300

    Add compiled assets and manifest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $manifestPath = public_path('build/manifest.json');
+    if (!file_exists($manifestPath)) {
+        $manifestPath = public_path('build/.vite/manifest.json');
+    }
+    
+    // If the file exists in the .vite folder, tell Laravel to use that
+    if (file_exists($manifestPath)) {
+        \Illuminate\Support\Facades\Vite::useManifestFilename(
+            str_contains($manifestPath, '.vite') ? '.vite/manifest.json' : 'manifest.json'
+        );
+    }
+    // \Illuminate\Support\Facades\Vite::useManifestFilename('manifest.json');
     }
 }
